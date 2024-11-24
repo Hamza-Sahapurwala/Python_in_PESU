bfff3fc9b60e1100d20f5d26dde906791ed2a5cd
Hamza-Sahapurwala <hamjuzyt@gmail.com>
2024-11-24 16:48:22 +0530

    * & ** Operators in Functions

--- learningpython.py
+++ learningpython.py
@@ -1,6 +1,8 @@
 
 # * Lovely Programs
 
+import csv
+
 '''print('Hello World!!')
 
 # This is test try for python
@@ -447,7 +449,7 @@ with open('t.csv','r',newline='\r\n') as d:
     for i in a:
 
         print(i)
-'''
+
 # ! Functions
 
 def areaofrectangle(a, b):
@@ -491,13 +493,13 @@ def a(a,b,c):
 
 print(a(1,2,3)) # * Returns a tuple
 
-'''
-There are 4 scenarios of Functions:
-With Argument, With return
-With Argument, Without return
-Without Argument, Without return
-Without Argument, With return
-'''
+
+# There are 4 scenarios of Functions:
+# With Argument, With return
+# With Argument, Without return
+# Without Argument, Without return
+# Without Argument, With return
+
 
 # ! Positional and Keyword Arguments
 
@@ -523,4 +525,86 @@ def aa(x,y):
 
 x = 3; y = 4 # * ; acts as a separator so that x can have 3 as value and y has value 4
 
-aa(x,y)
+# aa(x,y)
+
+a = eval(input('List:'))
+
+print(type(a))
+'''
+def bbb(*args):
+
+    # * args returns a tuple
+
+    print(args)
+
+    print('With *:',*args) # * Returns the input but can't do anything with that
+
+    # * args by itself is a tuple but *args is just a starred expression
+
+    # for i in *args: # * This doesn't work
+
+        # print(i)
+
+    for i in args:
+
+        print(i)
+
+bbb('A','B','C','D')
+
+def myFun(arg1, **kwargs):
+    
+    # * kwargs is now a dictionary with the 'before =' as the keys and 'after=' as the values
+
+    for key, value in kwargs.items():
+
+        print("%s == %s" % (key, value))
+
+# Driver code
+
+myFun("Hi", first='Geeks', mid='for', last='Geeks')
+
+def myFun(*args, **kwargs):
+
+    print("args: ", args)
+
+    # print('**kwargs: ',**kwargs) # * This doesn't work as all the first="Geeks", mid="for", last="Geeks" thing is just copy pasted and print is a function
+
+    print("kwargs: ", kwargs)
+
+# * Now we can use both *args ,**kwargs
+
+# * to pass arguments to this function :
+
+myFun('geeks', 'for', 'geeks', first="Geeks", mid="for", last="Geeks")
+
+def myFun(arg1, arg2, arg3):
+
+    print("arg1:", arg1) # * for kwargs: arg1 = for
+
+    print("arg2:", arg2) # * for kwargs: arg2 = Geeks
+
+    print("arg3:", arg3) # * for kwargs: arg3 = Geeks
+
+# * Now we can use *args or **kwargs to
+
+# * pass arguments to this function :
+
+args = ("Geeks", "for", "Geeks")
+
+myFun(*args)
+
+kwargs = {"arg3": "Geeks", "arg1": "for", "arg2": "Geeks"}
+
+myFun(**kwargs) # * the positional parameter in the function should have the same name
+
+# * as the keys in the dictionary named kwargs so that some output is achieved 
+
+def f1(a,b,*ar):
+
+    print(a,b,*ar) # * a = 12, b = 22, *ar = 2 6
+
+    # * prints 12 22 2 6
+
+f1(12,22,2,6)
+
+# * * and ** make is easier to take tuple and dictionary input
