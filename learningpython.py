5dbd2eca5047540e9e3b55c3e080fd573524ef98
Hamza-Sahapurwala <hamjuzyt@gmail.com>
2024-12-03 08:44:04 +0530

    Closures Pt.2

--- learningpython.py
+++ learningpython.py
@@ -887,3 +887,95 @@ d1(20) # * This will be 20 // 2
 d2(96) # * This will be 96 // 3
 
 print('d1:',d1,'\nd2:',d2) # * d1 and d2 are different as the outer function parameters are different
+
+# ! Closures Pt.2
+
+# * Add 2 no. using Closures
+
+def a(x): # * Outer
+
+    def add(y): # * Inner
+
+        print(x + y)
+
+    return add
+
+a1 = a(2) # * a1 is now the function where x = 2
+
+a2 = a(3) # * a2 is now the function where x = 3
+
+a1(4)
+
+a2(2)
+
+# * Multiply two no. using Closures
+
+def m(x): # * Outer
+
+    def multiply(y): # * Inner
+
+        print(x * y)
+
+    return multiply
+
+m1 = m(3) # * Instance of function multiply where x = 3 
+
+m2 = m(4) # * Instance of function multiply where x = 4
+
+m1(5)
+
+m2(5)
+
+# * Greeting someone using Closures
+
+def g():
+
+    def greet():
+
+        print('Hello')
+
+    return greet
+
+g1 = g() # * g1 is now the instance of function greet 
+
+g1()
+
+# * Find the all the odd no. until n using Closures
+
+def o(n): # * Outer
+
+    def odd(): # * Inner
+
+        for i in range(1,n+1,2):
+
+            print(i)
+
+    return odd
+
+o1 = o(10) # * o1 becomes the instance of the function odd() where n = 10
+
+o1()
+
+# ! nonlocal is a keyword that has to be studied
+
+def compute(): # * Outer
+
+    num = 1
+
+    def inner(): # * Inner
+
+        # * To change the value of num freely, use nonlocal keyword
+
+        nonlocal num
+
+        num +=1 # * We can't change the value of num, we can only access it if we don't use nonlocal keyword
+
+        # * This is an example of nonlocal
+
+        return num
+
+    return inner
+
+odd = compute()
+
+print(odd())
