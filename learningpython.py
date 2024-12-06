5e54533bba19c97c9ff47895c6119d86caaa0c6e
Hamza-Sahapurwala <hamjuzyt@gmail.com>
2024-12-06 13:01:08 +0530

    Decorators

--- learningpython.py
+++ learningpython.py
@@ -553,7 +553,7 @@ bbb('A','B','C','D')
 
 def myFun(arg1, **kwargs):
     
-    # * kwargs is now a dictionary with the 'before =' as the keys and 'after=' as the values
+    # * kwargs is now a dictionary with the 'before =' as the keys and '=after' as the values
 
     for key, value in kwargs.items():
 
@@ -1015,3 +1015,102 @@ a()
 # * The function still works even after a1 is deleted
 
 # * That means b1 stores the instance where a1 had the assigned value
+
+# ! Decorators
+
+# * Decorators are functions 
+
+# * used to modify the behaviour or result of another function
+
+# * Basically, we don't want to change the source code of the original function
+
+# * So we use another function to change some part, like the way arguments are given,etc.
+
+# * to change the result given by the og function 
+
+# * Example: We are only trying to divide some big no. by small no. so even if arguments are placed wrong, we just get what we want
+
+def division(a,b):
+
+    return a / b
+
+def smart_div(func): # * Decorator
+
+    def inner(a,b):
+
+        if a < b:
+
+            a, b = b, a
+
+        return func(a, b) # * func is division
+
+    return inner # * Callback
+
+div1 = smart_div(division) # *div1 is now inner
+
+print(div1(2, 4))
+
+
+# * @ Symbol Usage for Decoration
+
+def func_decorator(func):
+
+    def inner_func():
+
+        print('Before func_hello is called:')
+
+        func()
+
+        print('After func_hello')
+
+    return inner_func
+
+@func_decorator # * This needs a function defined below it and the caller of that function
+
+def func_hello():
+
+    print('Hello')
+
+func_hello()
+
+# * Using @, we avoid the 2 lines we used to write previously
+
+# h = func_decorator(func_hello)
+
+# h()
+
+def calculate(f):
+
+    def inn(*a):
+
+        print('Decorator')
+
+        f(*a)
+
+        print('End')
+
+    return inn
+
+@calculate
+
+def fac(n):
+
+    print(n)
+
+@calculate
+
+def maxi(a,b,c):
+
+    print(max(a,b,c))
+
+@calculate
+
+def mini(a,b,c,d):
+
+    print(min(a,b,c,d))
+
+fac(15)
+
+maxi(1,2,3)
+
+mini(1,2,3,4)
