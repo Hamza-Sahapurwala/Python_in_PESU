93ae9666ae565a0b1dffa2c3a05029618fa09624
Hamza-Sahapurwala <hamjuzyt@gmail.com>
2024-12-20 12:48:22 +0530

    __doc__, help() and __name__ Variables

--- modulelearning.py
+++ modulelearning.py
@@ -40,3 +40,33 @@ a += 1 # * As the variable is present in the trialmodule file, now error is rais
 # * When we comment the code of t.a above
 
 print(a)
+
+# ! __doc__ variable and help()
+
+# * In Python, each object can be documented using Docstrings
+
+# * e.g.
+
+def adddddd():
+
+    '''Performing Addition of two numbers'''
+    
+    print(a + 7)
+
+print('Using __doc__, we get:')
+
+print(adddddd.__doc__) # * Prints the triple quoted comments in the function
+
+# ! The triple quotes should always be on top
+
+print('Using help(), we get:')
+
+help(adddddd) # * This by itself prints the triple quoted comment with more things which tells where the function is present
+
+# ! __name__
+
+# * If no modules is imported, __name__ = __main__
+
+# * If modules are imported, __name__ = name of the file
+
+print(__name__) # * Prints __main__
