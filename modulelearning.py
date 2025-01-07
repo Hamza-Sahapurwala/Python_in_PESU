d659d24c6e913007e5c3e4161964d32bc02779ce
Hamza-Sahapurwala <hamjuzyt@gmail.com>
2025-01-07 19:40:56 +0530

    Inheritance in Python

--- modulelearning.py
+++ modulelearning.py
@@ -114,175 +114,175 @@
 
 # * lambda multiple arguments seperated by comma : expression which leads to output
 
-square = lambda x: x * x
+# square = lambda x: x * x
 
-print(square(4), square,type(square)) # * Prints 16 lambda smt <class 'function'>
+# print(square(4), square,type(square)) # * Prints 16 lambda smt <class 'function'>
 
-maxi = lambda a,b: a if a > b else b
+# maxi = lambda a,b: a if a > b else b
 
-print('The max is',maxi(3,2))
+# print('The max is',maxi(3,2))
 
-# * Lambda can also be used as an inner function
+# # * Lambda can also be used as an inner function
 
-def funky(n):
+# def funky(n):
 
-    return lambda a: a * n # * The innerf function
+#     return lambda a: a * n # * The innerf function
 
-innerboi = funky(4) # * Very similar to callback functions
+# innerboi = funky(4) # * Very similar to callback functions
 
-print(innerboi(3))
+# print(innerboi(3))
 
-# ! Map() Function
+# # ! Map() Function
 
-# * Used to apply one function to the individual elements of any variables
+# # * Used to apply one function to the individual elements of any variables
 
-# * Syntax: variable = map(function, iterable(s),...)
+# # * Syntax: variable = map(function, iterable(s),...)
 
-a = (1, 2, 3, 4)
+# a = (1, 2, 3, 4)
 
-result = map(lambda x: x + x, a) # * We are adding each individual element by themselves
+# result = map(lambda x: x + x, a) # * We are adding each individual element by themselves
 
-print(result, list(result)) # * Shows that result is a map object and list(result) returns us the list where the lambda function has been applied
+# print(result, list(result)) # * Shows that result is a map object and list(result) returns us the list where the lambda function has been applied
 
-n1 = [4, 2, 9]
+# n1 = [4, 2, 9]
 
-n2 = [5, 7, 5]
+# n2 = [5, 7, 5]
 
-r = list(map(lambda x,y: x+y, n1, n2)) # * Adds individual elements of two lists then converting it into a list
+# r = list(map(lambda x,y: x+y, n1, n2)) # * Adds individual elements of two lists then converting it into a list
 
-print(r)
+# print(r)
 
-# ! Filter Function
+# # ! Filter Function
 
-# * If we want to remove some elements from an iterable, we can use do it in one line
+# # * If we want to remove some elements from an iterable, we can use do it in one line
 
-# * We can still keep the iterable
+# # * We can still keep the iterable
 
-m = [59, 78, 90, 43]
+# m = [59, 78, 90, 43]
 
-r = list(filter(lambda x: x >= 70, m)) # * We want elements which are above 70
+# r = list(filter(lambda x: x >= 70, m)) # * We want elements which are above 70
 
-print(r)
+# print(r)
 
-# * Checking if vowels exist in a given word
+# # * Checking if vowels exist in a given word
 
-a = list('hello')
+# a = list('hello')
 
-r = list(filter(lambda x : x in 'aeiou', a))
+# r = list(filter(lambda x : x in 'aeiou', a))
 
-print(r)
+# print(r)
 
-# * To check if no.s are divisible by 3
+# # * To check if no.s are divisible by 3
 
-a = [1, 2, 3, 4, 5, 6]
+# a = [1, 2, 3, 4, 5, 6]
 
-print(list(filter(lambda x:x%3==0,a)))
+# print(list(filter(lambda x:x%3==0,a)))
 
-# * Example of using both map and filter
+# # * Example of using both map and filter
 
-a = ['Ram', 'Tejas', 'Aditya', 'Ravi', 'Dinesh', 'Raghu']
+# a = ['Ram', 'Tejas', 'Aditya', 'Ravi', 'Dinesh', 'Raghu']
 
-print(list(map(str.upper, filter(lambda x : len(x) > 5, a))))
+# print(list(map(str.upper, filter(lambda x : len(x) > 5, a))))
 
-# * For readability, we can write the above code as
+# # * For readability, we can write the above code as
 
-r = filter(lambda x:len(x)> 5, a) # * Names with length above 5 are used
+# r = filter(lambda x:len(x)> 5, a) # * Names with length above 5 are used
 
-rr = map(str.upper, r) # * Used to uppercase the names we got above
+# rr = map(str.upper, r) # * Used to uppercase the names we got above
 
-print(list(rr))
+# print(list(rr))
 
-# ! Reduce Function
+# # ! Reduce Function
 
-# * Used to make an iterable into 1 value
+# # * Used to make an iterable into 1 value
 
-# * Part of functools module
+# # * Part of functools module
 
-# * Syntax: reduce(function, iterable[,initializer])
+# # * Syntax: reduce(function, iterable[,initializer])
 
-# * function must take 2 arguments as first argument is accumalated value and second is another element of the iterable
+# # * function must take 2 arguments as first argument is accumalated value and second is another element of the iterable
 
-# * iterable is any iterable
+# # * iterable is any iterable
 
-# * initializer is used to as the first thing to add, subtract or multiply the iterable[0]
+# # * initializer is used to as the first thing to add, subtract or multiply the iterable[0]
 
-# * e.g
+# # * e.g
 
-import functools
+# import functools
 
-a = [11, 22, 33, 44, 55]
+# a = [11, 22, 33, 44, 55]
 
-r = functools.reduce(lambda x,y: x * y, a, 100) # * 100 is an initializer i.e. It is multiplied to the first element and then each element is multiplied to each other
+# r = functools.reduce(lambda x,y: x * y, a, 100) # * 100 is an initializer i.e. It is multiplied to the first element and then each element is multiplied to each other
 
-print(r)
+# print(r)
 
-r = functools.reduce(lambda x,y: x * y, a)
+# r = functools.reduce(lambda x,y: x * y, a)
 
-print(r)
+# print(r)
 
-# * Finding Factorial
+# # * Finding Factorial
 
-n = 5
+# n = 5
 
-print('Finding Factorial using reduce():',functools.reduce(int.__mul__, range(1, n + 1)))
+# print('Finding Factorial using reduce():',functools.reduce(int.__mul__, range(1, n + 1)))
 
-# * Finding sum till n
+# # * Finding sum till n
 
-n = 10
+# n = 10
 
-print('Adding all elements in the list:',functools.reduce(int.__add__, range(n)))
+# print('Adding all elements in the list:',functools.reduce(int.__add__, range(n)))
 
-# * To find max element
+# # * To find max element
 
-a = [11, 22, 33, 44, 55]
+# a = [11, 22, 33, 44, 55]
 
-print('Max no.:',functools.reduce(lambda x,y: x if x > y else y, a))
+# print('Max no.:',functools.reduce(lambda x,y: x if x > y else y, a))
 
-# ! List Comprehensions
+# # ! List Comprehensions
 
-# Syntax: listvariable = [expression for <variable> in <iterable> [if condition]]
+# # Syntax: listvariable = [expression for <variable> in <iterable> [if condition]]
 
-l = [i for i in range(11) if i%2==0] # * Even no. are put in the list
+# l = [i for i in range(11) if i%2==0] # * Even no. are put in the list
 
-l = [i ** 3 for i in range(11)] # * Cube of no. till 10 are put in list
+# l = [i ** 3 for i in range(11)] # * Cube of no. till 10 are put in list
 
-words = ['Hi','how','are','you']
+# words = ['Hi','how','are','you']
 
-items = [i[-1] for i in words] # * Last letter of each word is put in the list
+# items = [i[-1] for i in words] # * Last letter of each word is put in the list
 
-l1 = [1,2,3,4]
+# l1 = [1,2,3,4]
 
-l2 = [2,3,4,5]
+# l2 = [2,3,4,5]
 
-print([a for a in l1 for b in l2 if a == b]) # * To print similar elements
+# print([a for a in l1 for b in l2 if a == b]) # * To print similar elements
 
-city = ['Bangalore', 'Mysore', 'Chennai', 'Hyderabad']
+# city = ['Bangalore', 'Mysore', 'Chennai', 'Hyderabad']
 
-a = [[i,len(i)] for i in city] # * City with their length
+# a = [[i,len(i)] for i in city] # * City with their length
 
-print(a)
+# print(a)
 
-a = [i ** 2 for i in range(11) if i%2==1] # * Odd sqaures are printed
+# a = [i ** 2 for i in range(11) if i%2==1] # * Odd sqaures are printed
 
-print(a)
+# print(a)
 
-# * Transpose of a matrix
+# # * Transpose of a matrix
 
-mat = [[1,2], [3,4], [5,6], [7,8]]
+# mat = [[1,2], [3,4], [5,6], [7,8]]
 
-transpose = [[row[i] for row in mat] for i in range(2)]
+# transpose = [[row[i] for row in mat] for i in range(2)]
 
-# ! Zip Functions
+# # ! Zip Functions
 
-# * Used to go through multiple lists together
+# # * Used to go through multiple lists together
 
-# * For two lists, it groups the individual elements in a tuple which is another iterable(which we can specify)
+# # * For two lists, it groups the individual elements in a tuple which is another iterable(which we can specify)
 
-a = [1, 2, 3]
+# a = [1, 2, 3]
 
-b = [4, 5, 6]
+# b = [4, 5, 6]
 
-print(list(zip(a, b)), dict(zip(a,b))) # * We can also use dictionaries with zip
+# print(list(zip(a, b)), dict(zip(a,b))) # * We can also use dictionaries with zip
 
 # ! Classes and Objects
 
@@ -333,3 +333,43 @@ print(getattr(car1, 'r')) # * Says which value has changed
 print(hasattr(car1, 'r')) # * Returns True if variable is present in class else False
 
 # delattr(car1, 'r') # * removes element r
+
+# ! Inheritance
+
+# * Where one class takes or 'inherits' attributes(variables) or methods(functions) from other classes
+
+class Curry:
+
+    def __init__(self, name, temperature):
+        
+        print('In Class Curry Constructor')
+
+        self.name = name
+
+        self.temperature = temperature
+
+    def display(self):
+
+        print('In Class Curry Display')
+
+class Vegetable(Curry): # * This leads to inheritance
+
+    def __init__(self, name, temperature, batchno):
+
+        self.batchno = batchno
+        
+        Curry.__init__(self, name, temperature) # * We can initialize the other variables using the other Parent's class's Constructor
+
+        print(f'Taken from other class:\nName of Soup: {self.name}\nTemperature of Soup: {self.temperature}\nIs in this Vegetable Class:\nBatchno: {self.batchno}')
+
+    def disp(self):
+        
+        print('In Class Vegetable Disp')
+
+        Curry.display(self) # * We can use the Parent Class to call it's function
+
+eng = Vegetable('Tomato Soup', 56, 101) # * When I call this, the constructor of Curry runs first
+
+# eng.disp() # * Can call the method in this class itself
+
+# eng.display() # * Can also call the method present inside Parent Class
