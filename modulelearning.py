e4cf99eed0fa85ff9a28fd2ecf055bdc34740887
Hamza-Sahapurwala <hamjuzyt@gmail.com>
2025-01-07 15:37:28 +0530

    Zip Function

--- modulelearning.py
+++ modulelearning.py
@@ -271,3 +271,15 @@ print(a)
 mat = [[1,2], [3,4], [5,6], [7,8]]
 
 transpose = [[row[i] for row in mat] for i in range(2)]
+
+# ! Zip Functions
+
+# * Used to go through multiple lists together
+
+# * For two lists, it groups the individual elements in a tuple which is another iterable(which we can specify)
+
+a = [1, 2, 3]
+
+b = [4, 5, 6]
+
+print(list(zip(a, b)), dict(zip(a,b))) # * We can also use dictionaries with zip
